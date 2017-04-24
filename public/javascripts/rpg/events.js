70dcf13e5a76a8388d91c92732c9be9e1ba88183
Garett Haight <garett.haight@gmail.com>
2017-04-23 17:02:42 -0700

    Added support for conditional events (door), better support for naming items, updated player sprite

--- public/javascripts/rpg/events.js
+++ public/javascripts/rpg/events.js
@@ -19,10 +19,52 @@ class Events{
 					return "Moving to a new map";
 				}
 			},
+			door(args, game) {
+				// if door is locked check if unlock condition is met, then unlock
+				if (args.properties.locked) {
+					if(args.properties.unlock) {
+						var uCondition = JSON.parse(args.properties.unlock);
+						if(uCondition.item) {
+							if (game.player.inventory[uCondition.item] >= uCondition.qty) {
+								// unlock door permanently
+								args.properties.unlock = false;
+								// remove from inventory
+								game.player.inventory[uCondition.item] -= uCondition.qty;
+								return this.teleport(args, game);
+							}
+							else {
+								// You don't meet the unlock condition
+								// TODO: Maybe check for custom message here?
+								// TODO: These message strings might should be in a global bank, so they're easier to change
+								return "Locked!";
+							}
+						}
+						else {
+							// unknown unlock condition. Probably author error
+							return "This door cannot be opened.";
+						}
+					}
+					else if (args.properties.unlock === false) {
+						// Door has been unlocked
+                        return this.teleport(args, game);
+                    }
+                    else {
+						// no unlock condition found? Probably author error
+						return "Locked!";
+					}
+				}
+				else {
+					// Door is not locked, never was. Carry on.
+					return this.teleport(args, game);
+				}
+			},
 			item(args, game) {
 				var itemCount = 0;
 				var props = args.properties;
 				var inv = game.player.inventory;
+
+				// item name defaults to key value if no name is provided
+				var itemName = props.name ? props.name : props.item;
 				if(typeof inv[props.item] === 'undefined') {
 					inv[props.item] = 0;
 				}
@@ -51,16 +93,15 @@ class Events{
 								game.map.events.splice(index, 1);
 							}
 						}
-						return `Added (${itemCount}) of ${props.item}`;
+						return `Added (${itemCount}) of ${itemName}`;
 					}
 					else {
 						// change quantity 
-						return `Added (${itemCount}) of ${props.item}`;
+						return `Added (${itemCount}) of ${itemName}`;
 					}
-					
 				}
 				else {
-					return `Can't carry anymore of ${props.item}!`;
+					return `Can't carry anymore of ${itemName}!`;
 				}
 			}
 		};
