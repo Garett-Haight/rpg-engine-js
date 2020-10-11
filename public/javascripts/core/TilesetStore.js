d32ae9adc917168a80f219fc99c436723cda3cf0
Garett Haight <garett.haight@gmail.com>
2020-10-10 19:05:01 -0700

    foramatting, gid method stub, remove lodash

--- public/javascripts/core/TilesetStore.js
+++ public/javascripts/core/TilesetStore.js
@@ -1,38 +1,52 @@
-import TilesetService from './services/TilesetService'
+import TilesetService from "./services/TilesetService";
 
 class TilesetStore {
-    constructor() { 
-        if (!TilesetStore.instance) {
-            this._tilesets = [];
-            TilesetStore.instance = this;
+  constructor() {
+    if (!TilesetStore.instance) {
+      this._tilesets = [];
+      this._gidMap = new Map();
+      TilesetStore.instance = this;
+    }
+    return TilesetStore.instance;
+  }
+
+  add(tileset) {
+    tileset.id = this._tilesets.length;
+    this._tilesets.push(tileset);
+    return tileset;
+  }
+
+  exists(tileset) {
+    return this._tilesets.find((ts) => {
+      if (typeof tileset == "string") {
+        return ts.getName() == tileset;
+      } else {
+        return ts.getName() == tileset.getName();
+      }
+    });
+  }
+
+  get(name) {
+    // Fetch from service if not found
+    return this._tilesets.find((ts) => {
+      return ts.getName() == name;
+    });
+  }
+
+  getGlobalGidMap() {
+    return this._gidMap;
+  }
+
+  getGidMapForGameMap(tileset, map) {
+    if (this._gidMap.has(tileset)) { // TODO: update gid heirarchy
+        let ts = this._gidMap.get(tileset);
+        if (ts.has(map)) {
+            return ts.get(map);
         }
-        return TilesetStore.instance;          
     }
-
-    add(tileset) {
-        tileset.id = this._tilesets.length;
-        this._tilesets.push(tileset);
-        return tileset;
-    }
-
-    exists(tileset) {
-        return this._tilesets.find((ts) => {
-            if (typeof tileset == 'string') {
-                return ts.getName() == tileset;
-            }
-            else {
-                return ts.getName() == tileset.getName();
-            }
-        });
-    }
-
-    get(name) {
-        // Fetch from service if not found
-        return this._tilesets.find((ts) => {
-            return ts.getName() == name;
-        });
-    }
-};
+    return null;
+  }
+}
 
 const instance = new TilesetStore();
 Object.freeze(instance);
