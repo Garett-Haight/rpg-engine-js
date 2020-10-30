2453dc9a5f81c7fecab51faa2e226f41bbb39a7d
Garett Haight <garett.haight@gmail.com>
2020-10-29 20:36:38 -0700

    just some notes

--- public/javascripts/core/Renderer.js
+++ public/javascripts/core/Renderer.js
@@ -9,6 +9,14 @@ class Renderer {
 		if(!Renderer.instance) {
 			Renderer.instance = this;
 			this.ctx = ctx;
+			this.queue = [];
+			// sample queue element
+			// {region: {x, y}, callbackFn}
+			// is there a way to make a generic renderable object that can be passed to the queue?
+			// might be best to wait until visible elements are determined
+			// render queuing could be used for 'backface culling' to eliminate unnecessary render operations
+			// a compositing tree may be useful for determining visibility
+			// https://nothings.org/gamedev/compositing_tree/
 		}
 		return Renderer.instance;
 	};
---
+++ public/javascripts/core/dataTypes/graph.js
@@ -0,0 +1,9 @@
+class Node {
+    constructor() {
+        this.edges = [];
+    }
+}
+
+class Graph {
+
+}
